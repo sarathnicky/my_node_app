a555fbffb26ffcda1f0fee0e1af83e207d82df99
sarathkumar <sarathkumar@realtekh.com>
2020-02-10 13:05:08 +0530

    sarath

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/*console.log("working!");
-var mysql = require('mysql')
-
-var connection = mysql.createConnection({
-        host: 'zintzotek.com',
-        user: 'zintzotek1',
-        password: 'realtek@2019',
-        database: 'zintzote_nodejs'
-    });
-	
-	
-	connection.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-});*/
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
