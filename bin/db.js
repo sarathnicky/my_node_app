683d74c8c65e87e780ce0ca0c5f5a62644634f1a
sarathnicky <sarath.nicky0626@gmail.com>
2020-02-10 18:19:01 +0530

    Update db.js

--- bin/db.js
+++ bin/db.js
@@ -7,19 +7,19 @@ var mysql = require('mysql')
 //     database: 'nodejs'
 // });
 
-    /*var connection = mysql.createConnection({
+    var connection = mysql.createConnection({
         host: 'zintzotek.com',
         user: 'zintzotek1',
         password: 'realtek@2019',
         database: 'zintzote_nodejs'
-    });*/
+    });
 	
-	var connection = mysql.createConnection({
+	/*var connection = mysql.createConnection({
 	 host: 'remotemysql.com',
     user: 'UTTr1h1raL',
     password: 'XmLNj4q9of',
     database: 'UTTr1h1raL'
-	});
+	});*/
 
 
 
